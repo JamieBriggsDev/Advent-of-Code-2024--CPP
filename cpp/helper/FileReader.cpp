4fa016b762783bb18579e9c8e70ac81b8df16e57
Jamie Briggs <jamie.briggs@opencastsoftware.com>
2024-11-30 20:02:34 +0000

    Optimized FileReader.cpp

--- cpp/helper/FileReader.cpp
+++ cpp/helper/FileReader.cpp
@@ -14,17 +14,18 @@ using namespace std;
 using namespace helper;
 
 SolutionInput* FileReader::readFile(const string& fileLocation) {
-  vector<string> outputList;
-  outputList.clear();
+  vector<string> solutionInput;
+  if (FILE *fp = fopen(fileLocation.c_str(), "r")) {
+    char row[500];
 
-  ifstream myFile(fileLocation);
-
-  string s;
-  while (getline(myFile, s)) {
-    outputList.push_back(s);
+    // This reads a line up to 500 characters long
+    while (fscanf(fp, "%500s", &row) == 1) {
+      solutionInput.emplace_back(row);
+    }
+    fclose(fp);
   }
 
-  return new SolutionInput(outputList);
+  return new SolutionInput(solutionInput);
 }
 
 bool FileReader::fileExists(const std::string& fileLocation) {
