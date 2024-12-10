eafbb60996ede24171166df0555e821a58ba16ed
Jamie Briggs <personal@jbriggs.dev>
2024-12-10 23:32:43 +0000

    Created IncrementalPathFinder

---
+++ cpp/core/Node.cpp
@@ -0,0 +1,8 @@
+//
+// Created by perso on 10/12/2024.
+//
+
+#include "Node.h"
+
+namespace core {
+} // day10
---
+++ cpp/core/Node.h
@@ -0,0 +1,35 @@
+//
+// Created by perso on 10/12/2024.
+//
+
+#ifndef PATH_H
+#define PATH_H
+#include "../core/Pair.h"
+
+#include <vector>
+
+namespace core {
+
+class Node : public Pair{
+  int weight;
+  // Pointer to all parent nodes (Parent being towards start
+  std::vector<Node*> parentNodes;
+public:
+  Node(int height, int x, int y) : Pair(x, y), weight(height) {}
+  int getHeight() const {
+    return this->weight;
+  }
+  void addParent(Node* path) {
+    this->parentNodes.push_back(path);
+  }
+  std::vector<Node *> getParentNodes() const { return this->parentNodes; }
+  friend bool operator==(const Node &lhs, const Node &rhs) {
+    return static_cast<const Pair &>(lhs) == static_cast<const Pair &>(rhs) && lhs.weight == rhs.weight &&
+           lhs.parentNodes == rhs.parentNodes;
+  }
+  friend bool operator!=(const Node &lhs, const Node &rhs) { return !(lhs == rhs); }
+};
+
+} // day10
+
+#endif //PATH_H
