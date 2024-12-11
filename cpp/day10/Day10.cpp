a7d6734620c936a0309a5a2b583058f674820fd0
Jamie Briggs <personal@jbriggs.dev>
2024-12-11 00:11:39 +0000

    Day 10 part 2 solved

--- cpp/day10/Day10.cpp
+++ cpp/day10/Day10.cpp
@@ -19,11 +19,11 @@ namespace solutions {
     // Get all starting positions
     auto nodes = map.getStartingPositions();
     // Get all paths from positions
-    vector<queue<core::Node*>> allPaths;
+    vector<queue<core::Node *>> allPaths;
 
     core::IncrementalPathFinder pathFinder;
 
-    std::map<core::Node*, int> trailHeadPoints;
+    std::map<core::Node *, int> trailHeadPoints;
 
     for (auto node: nodes) {
       // First add node to trailHeadPoints
@@ -33,10 +33,10 @@ namespace solutions {
       // Next find all possible paths with starting node
       auto paths = pathFinder.findAllPaths(node);
       // Loop through each path, and find ones which end on a peak (i.e size of 10)
-      for(auto path : paths) {
+      for (auto path: paths) {
         if (path.size() == 10) {
-          core::Node* destination = path.back();
-          if(!finishingNodes.contains(destination)) {
+          core::Node *destination = path.back();
+          if (!finishingNodes.contains(destination)) {
             finishingNodes.insert(destination);
             trailHeadPoints[node]++;
           }
@@ -52,5 +52,36 @@ namespace solutions {
 
     return std::to_string(sum);
   }
-  std::string Day10::solvePartTwo(const helper::SolutionInput *input) { return "To Solve"; }
-} // solutions
+  std::string Day10::solvePartTwo(const helper::SolutionInput *input) {
+    core::TopographicMap map = core::TopographicMap(input->getTestInput());
+    // Get all starting positions
+    auto nodes = map.getStartingPositions();
+    // Get all paths from positions
+    vector<queue<core::Node *>> allPaths;
+
+    core::IncrementalPathFinder pathFinder;
+
+    std::map<core::Node *, int> trailHeadPoints;
+
+    for (auto node: nodes) {
+      // First add node to trailHeadPoints
+      trailHeadPoints[node] = 0;
+      // Next find all possible paths with starting node
+      auto paths = pathFinder.findAllPaths(node);
+      // Loop through each path, and find ones which end on a peak (i.e size of 10)
+      for (auto path: paths) {
+        if (path.size() == 10) {
+          trailHeadPoints[node]++;
+        }
+      }
+    }
+
+    // Find sum of all values in trailHeadPoints
+    int sum = 0;
+    for (const auto &pair: trailHeadPoints) {
+      sum += pair.second;
+    }
+
+    return std::to_string(sum);
+  }
+} // namespace solutions
