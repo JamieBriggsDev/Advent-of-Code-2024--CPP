04f835eac0096f40f3a14df7556d97bf9425493e
Jamie Briggs <personal@jbriggs.dev>
2024-12-15 20:31:13 +0000

    Day 7 README.md added

---
+++ cpp/day7/Equation.cpp
@@ -0,0 +1,8 @@
+//
+// Created by perso on 15/12/2024.
+//
+
+#include "Equation.h"
+
+namespace solutions {
+} // solutions
---
+++ cpp/day7/Equation.h
@@ -0,0 +1,16 @@
+//
+// Created by perso on 15/12/2024.
+//
+
+#ifndef EQUATION_H
+#define EQUATION_H
+
+namespace solutions {
+
+class Equation {
+
+};
+
+} // solutions
+
+#endif //EQUATION_H
