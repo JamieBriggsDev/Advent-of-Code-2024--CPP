c1f780c81c862f4fa72ee04db574ba163d01ab9c
Jamie Briggs <personal@jbriggs.dev>
2024-12-18 00:21:36 +0000

    Day 17: Started on part 2

--- cpp/day17/Day17.cpp
+++ cpp/day17/Day17.cpp
@@ -22,18 +22,40 @@ namespace solutions {
     return result;
   }
   std::string Day17::solvePartTwo(const helper::SolutionInput *input) {
+
     ChronospatialComputer computer(input->getTestInput());
-    computer.setRegisterAValue(117440);
-    vector<int> output = computer.run();
-    string result = "";
-    for (int i = 0; i < output.size(); i++) {
-      result += to_string(output[i]);
-      if (i < output.size() - 1) {
-        result += ",";
+    std::string program = "";
+    for (int i = 0; i < computer.getProgram().size(); i++) {
+      program += to_string(computer.getProgram()[i]);
+      if (i < computer.getProgram().size() - 1) {
+        program += ",";
+      }
+    }
+
+    int i = 0;
+    while (true) {
+      // Due to the modula 8 operation, taking an assumption and going up in 8s. This is
+      //   VERY brute force...
+      // Perhaps the solution to this is to work backwards?
+      i += 8;
+
+      ChronospatialComputer computerToCheck(input->getTestInput());
+      computerToCheck.setRegisterAValue(i);
+      vector<int> output = computerToCheck.run();
+      string result = "";
+      for (int i = 0; i < output.size(); i++) {
+        result += to_string(output[i]);
+        if (i < output.size() - 1) {
+          result += ",";
+        }
+      }
+
+      if (result == program) {
+        return to_string(i);
       }
     }
 
     // Not 167430506
-    return result;
+    return "Not found :(";
   }
 } // namespace solutions
